831af50113f1a27405b41e4c11e1ad26f292dc30
Oleksandr Aleksieiev <oleksandr.aleksieiev@globallogic.com>
2021-11-21 00:55:58 +0200

    Task.01: Add datatypes and primitive main
    Signed-off-by: Oleksandr Aleksieiev <oleksandr.aleksieiev@globallogic.com>

---
+++ 01_git/rps/src/datatypes/constants.h
@@ -0,0 +1,13 @@
+/*
+* COPYRIGHT (C) 2021 Oleksandr Aleksieiev
+* all rights reserved
+*/
+
+#ifndef ROCK_PAPER_SCISSORS_CONSTANTS_H
+#define ROCK_PAPER_SCISSORS_CONSTANTS_H
+
+namespace rps {
+    constexpr char EXIT_OPTION = 'x';
+}
+
+#endif//ROCK_PAPER_SCISSORS_CONSTANTS_H
---
+++ 01_git/rps/src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * COPYRIGHT (C) 2021 Oleksandr Aleksieiev
+ * all rights reserved
+*/
+
+#include <iostream>
+
+#include "datatypes/constants.h"
+#include "driver/player_move.h"
+#include "ui/user_interface.h"
+
+using namespace rps;
+
+int main() {
+    std::cout << "bye" << std::endl;
+    return 0;
+}
+
