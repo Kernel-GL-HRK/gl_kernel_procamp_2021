93d1363347039b5a7933bfadb4d73e9d7240e446
Victor Krasnoshchok <ct3da21164@protonmail.ch>
2021-11-22 12:56:35 +0200

    TASK2: Fix more typos

--- 02_bash/hwdetect/hwdetect.sh
+++ 02_bash/hwdetect/hwdetect.sh
@@ -26,15 +26,17 @@ cleanup () {
 }
 
 filter() {
-    # tail + grep combo return a single block of strings separated with
+    # tail + grep combo returns a single block of strings separated with
     # spaces instead of '\n's. udevadm does not have an ability to filter
-    # events by action types by itself so, for example inserting a new
-    # USB device generates lots of output which contains also 'bind' actions
-    # wehich we consider just a noise for this task. These must be excluded
+    # events by action types by itself so, for example inserting/removing a
+    # USB device generates lots of output which contain also 'bind/unbind' actions
+    # which we consider just a noise for this task. These must be excluded
     # from the output, so we iterate through the list of words in search
-    # for the give tag (like 'add' or 'remove'); in case one is found
-    # the next token is device name (according to udevadm format). Sub-classes
+    # for the given tag (like 'add' or 'remove'); in case one is found
+    # the next token is the device name (according to udevadm format). Sub-classes
     # are filtered by --subsystem-match so we don't need to filter that lines.
+    # This also eats up [KERNEL... headers.
+    # The same could be probably achieved by awk?
 
     local tag=$1
     local tag_found=0
@@ -66,6 +68,10 @@ do
     diff_list=`tail -c +$read_pos $temp_buf`
     diff_len=${#diff_list}
     ((read_pos+=$diff_len))
+
+    # Preprocess add/remove lists in order not to
+    # filter empty input or an input which does not
+    # contain data which we're interested in.
     added_list=`echo $diff_list | grep add`
     removed_list=`echo $diff_list | grep remove`
 
