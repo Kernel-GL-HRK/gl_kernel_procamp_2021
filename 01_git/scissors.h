1762820785ab6b6ac467918e0882487c9e08276b
volodymyrkniazkyi <94599590+volodymyrkniazkyi@users.noreply.github.com>
2021-11-26 09:14:56 +0200

    01_git: Create rock-paper-scissors game (#57)
    Add initial revision of homework task

---
+++ 01_git/scissors.h
@@ -0,0 +1,22 @@
+﻿#ifndef __SCISSORS_H__
+#define __SCISSORS_H__
+
+/*
+ * Header file with configuration of
+ * scissors-paper project
+ */
+#define SYMB_EXIT      'e'
+#define SYMB_PAPER     'p'
+#define SYMB_ROCK      'r'
+#define SYMB_SCISSORS  's'
+
+#define STR_PAPER     "paper"
+#define STR_ROCK      "rock"
+#define STR_SCISSORS  "scissors"
+
+#define WINNER_1    1
+#define WINNER_2    2
+#define WINNER_NO   0
+#define WINNER_ERR  -1
+
+#endif
