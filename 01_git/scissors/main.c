ec42f8c092e72ca6e2e00885db1e909c96228065
Vadym.Hutovskyi <vadym.hutovskyi@globallogic.com>
2021-11-30 21:18:35 +0200

    TASK1: Modify a game end logic
    Add asking a question about playing again before the winner was defined
    
    Signed-off-by: Vadym.Hutovskyi <vadym.hutovskyi@globallogic.com>

--- 01_git/scissors/main.c
+++ 01_git/scissors/main.c
@@ -7,6 +7,7 @@
 #define CHAR_ROCK         ('r')
 #define CHAR_PAPER        ('p')
 #define CHAR_SCISSORS     ('s')
+#define CHAR_YES          ('y')
 #define BUF_LENGTH        (16u)
 
 int main()
@@ -44,18 +45,26 @@ int main()
 
         srand(time(NULL));
         ai = rand() % VARS_NUM;
-        printf("You choose %s, I choose %s\n", option[player], option[ai]);
+        printf("You choose %s, I choose %s\n\n", option[player], option[ai]);
 
         if (player == ai) {
-            printf("Draw... Try again!\n\n");
+            printf("Draw... There is no winner!\n\n");
+        } else {
+            if((player==ROCK && ai==SCISSORS) || (player==PAPER && ai==ROCK) || (player==SCISSORS && ai==PAPER)) {
+                printf("You win: %s beats %s\n\n", option[player], option[ai]);
+            } else {
+                printf("I win: %s beats %s\n\n", option[ai], option[player]);
+            }
         }
 
-        if ((player==ROCK && ai==SCISSORS) || (player==PAPER && ai==ROCK) || (player==SCISSORS && ai==PAPER)) {
-            printf("You win: %s beats %s\n\n", option[player], option[ai]);
+        printf("Do you want to play again? Type (%c) if YES or any key if NOT and press Enter\n", CHAR_YES);
+        fgets(buf, sizeof(buf), stdin);
+        if (buf[0] != CHAR_YES) {
+            game_active = 0;
         } else {
-            printf("I win: %s beats %s\n\n", option[ai], option[player]);
+            printf("\n");
         }
-        game_active = 0;
     }
+    printf("\nThanks for the great game!");
     return 0;
 }
