3b154d30d09344ec34c5c3aa1f8f3325d766c2d1
Andrey Berdnik <andrey.berdnik@gmail.com>
2021-12-01 15:22:05 +0200

    03_module - fix format with checkpatch

--- 03_module/hello.c
+++ 03_module/hello.c
@@ -1,18 +1,19 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-static int param_a=0;
-module_param(param_a,int,0660);
+static int param_a;
+module_param(param_a, int, 0660);
 
-static int param_b=0;
-module_param(param_b,int,0660);
+static int param_b;
+module_param(param_b, int, 0660);
 
 int init_module(void)
 {
-    printk(KERN_INFO "Hi world! Sum: %d\n", param_a+param_b);
-    return 0;
+	pr_info("Hi world! Sum: %d\n", param_a + param_b);
+	return 0;
 }
 
-void cleanup_module(void) {
-    printk(KERN_INFO "Bye world! Subtraction:%d\n", param_a-param_b);
+void cleanup_module(void)
+{
+	pr_info("Bye world! Subtraction:%d\n", param_a - param_b);
 }
