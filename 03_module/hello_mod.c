34fb31c1d30a9598483002a6e601699cfc86d271
SyedinSN <sergeynsn83@gmail.com>
2021-12-06 17:47:13 +0200

    TASK3: Add an additional function for entering parameters
    Added a file with the ability to enter parameters into the module after loading
    
    Signed-off-by: SyedinSN <sergeynsn83@gmail.com>

--- 03_module/hello_mod.c
+++ 03_module/hello_mod.c
@@ -1,41 +1,26 @@
+// SPDX-License-Identifier: MIT
+
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
 
-static int a=10;
-module_param(a,int,0660);
+static int a;
+module_param(a, int, 0660);
 
-static int b=15;
-module_param(b,int,0660);
-
-static int m;
-
+static int b;
+module_param(b, int, 0660);
 
 static int module_open(void)
 {
-	printk(KERN_INFO "Operetion summary\n");
-    printk(KERN_INFO "Parameter1=%d\n", a);
-    printk(KERN_INFO "Parameter2=%d\n", b);
-
-    m = a + b;
-
-    printk(KERN_INFO "Summ=%d\n", m);
-
+	pr_info("Addition process\n");
+	pr_info("Hi...P1=%d P2=%d Summ=%d\n", a, b, a+b);
 	return 0;
 }
-
-static void module_end(void) 
+static void module_end(void)
 {
-	printk(KERN_INFO "Operetion substration\n");
-    printk(KERN_INFO "Parameter1=%d\n", a);
-    printk(KERN_INFO "Parameter2=%d\n", b);
-    
-	m = a - b;
-
-    printk(KERN_INFO "Subst=%d\n", m);
+	pr_info("Processing substration\n");
+	pr_info("bye...P1=%d P2=%d Subst=%d\n", a, b, a-b);
 }
-
-
-module_init (module_open);
-module_exit (module_end);
+module_init(module_open);
+module_exit(module_end);
