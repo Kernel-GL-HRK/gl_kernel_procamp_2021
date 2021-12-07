ce9e01103a58c7d25ec2e3202b4c88f79bb7f4d6
Valentyn Korniienko <kornienko-vr@rambler.ru>
2021-12-07 01:22:38 -0800

    pr:applied clang-format to task05/task06

--- 05_timers/host_app/main.c
+++ 05_timers/host_app/main.c
@@ -6,49 +6,51 @@
 
 // https://linux.die.net/man/3/clock_gettime
 
-const char* clockid_to_string(clockid_t id)
+const char *clockid_to_string(clockid_t id)
 {
-    switch(id){
-
-        case CLOCK_REALTIME:
-            return "CLOCK_REALTIME";
-        break;
-        case CLOCK_MONOTONIC:
-            return "CLOCK_MONOTONIC";
-        break;
-        case CLOCK_PROCESS_CPUTIME_ID:
-            return "CLOCK_PROCESS_CPUTIME_ID";
-        break;
-        case CLOCK_THREAD_CPUTIME_ID:
-            return "CLOCK_THREAD_CPUTIME_ID";
-        break;
-        default:
-            return "UNDEFINED";
-        break;
-    }
-    
+	switch (id) {
+	case CLOCK_REALTIME:
+		return "CLOCK_REALTIME";
+		break;
+	case CLOCK_MONOTONIC:
+		return "CLOCK_MONOTONIC";
+		break;
+	case CLOCK_PROCESS_CPUTIME_ID:
+		return "CLOCK_PROCESS_CPUTIME_ID";
+		break;
+	case CLOCK_THREAD_CPUTIME_ID:
+		return "CLOCK_THREAD_CPUTIME_ID";
+		break;
+	default:
+		return "UNDEFINED";
+		break;
+	}
 }
 
 void show_time_source_info(clockid_t clock_id)
 {
-    struct timespec timespec;
-    memset(&timespec,0,sizeof(struct timespec));
+	struct timespec timespec;
+	memset(&timespec, 0, sizeof(struct timespec));
 
-    int result = clock_getres(clock_id,&timespec);
-    assert(!result);
+	int result = clock_getres(clock_id, &timespec);
+	assert(!result);
 
-    printf("Clocksource: %s, seconds:%ld, nanoseconds:%ld \n",clockid_to_string(clock_id), timespec.tv_sec, timespec.tv_nsec);
+	printf("Clocksource: %s, seconds:%ld, nanoseconds:%ld \n",
+	       clockid_to_string(clock_id), timespec.tv_sec, timespec.tv_nsec);
 
-    struct tm* absolute_time = localtime(&timespec.tv_sec);
-    printf("ABSOLUTE TIME: %d:%d:%d %d/%d/%d \n",absolute_time->tm_hour, absolute_time->tm_min, absolute_time->tm_sec, absolute_time->tm_mday,absolute_time->tm_mon,absolute_time->tm_year+1900);
+	struct tm *absolute_time = localtime(&timespec.tv_sec);
+	printf("ABSOLUTE TIME: %d:%d:%d %d/%d/%d \n", absolute_time->tm_hour,
+	       absolute_time->tm_min, absolute_time->tm_sec,
+	       absolute_time->tm_mday, absolute_time->tm_mon,
+	       absolute_time->tm_year + 1900);
 }
 
 int main()
 {
-    show_time_source_info(CLOCK_REALTIME);
-    show_time_source_info(CLOCK_MONOTONIC);
-    show_time_source_info(CLOCK_PROCESS_CPUTIME_ID);
-    show_time_source_info(CLOCK_THREAD_CPUTIME_ID);
+	show_time_source_info(CLOCK_REALTIME);
+	show_time_source_info(CLOCK_MONOTONIC);
+	show_time_source_info(CLOCK_PROCESS_CPUTIME_ID);
+	show_time_source_info(CLOCK_THREAD_CPUTIME_ID);
 
-    return 0;
+	return 0;
 }
--- 06_memory/kernel_driver/memory_profiler.c
+++ 06_memory/kernel_driver/memory_profiler.c
@@ -10,92 +10,94 @@
 // https://elixir.bootlin.com/linux/latest/source/arch/sparc/kernel/traps_64.c#L905
 // https://www.kernel.org/doc/gorman/html/understand/understand009.html
 
-typedef enum
-{
-    k_kmalloc,
-    k_vmalloc,
-    k_get_free_pages,
-    k_kzalloc
+typedef enum {
+	k_kmalloc,
+	k_vmalloc,
+	k_get_free_pages,
+	k_kzalloc
 } alloc_profile_mode_t;
 
-const char *alloc_function_name[] = {"kmalloc", "kvmalloc", "get_free_pages", "kzalloc"};
+const char *alloc_function_name[] = { "kmalloc", "kvmalloc", "get_free_pages",
+				      "kzalloc" };
 
 static void profile_alloc(alloc_profile_mode_t profile_mode)
 {
+	cycles_t alloc_start_time;
+	cycles_t alloc_end_time;
+	cycles_t freeing_start;
+	cycles_t freeing_end;
 
-    cycles_t alloc_start_time;
-    cycles_t alloc_end_time;
-    cycles_t freeing_start;
-    cycles_t freeing_end;
+	char *kernel_mem = NULL;
 
-    char *kernel_mem = NULL;
+	size_t alloc_request = 1;
+	do {
+		unsigned long order = 1;
+		unsigned long psize;
+		for (psize = PAGE_SIZE; psize < alloc_request; psize *= 2) {
+			++order;
+		}
 
-    size_t alloc_request = 1;
-    do
-    {
-        unsigned long order = 1;
-        unsigned long psize;
-        for (psize = PAGE_SIZE; psize < alloc_request; psize *= 2)
-        {
-            ++order;
-        }
+		alloc_start_time = get_cycles();
+		switch (profile_mode) {
+		case k_kmalloc:
+			kernel_mem = (char *)kmalloc(alloc_request, GFP_KERNEL);
+			break;
+		case k_vmalloc:
+			kernel_mem = (char *)vmalloc(alloc_request);
+			break;
+		case k_get_free_pages:
+			kernel_mem =
+				(char *)__get_free_pages(GFP_KERNEL, order);
+			break;
+		case k_kzalloc:
+			kernel_mem = (char *)kzalloc(alloc_request, GFP_KERNEL);
+			break;
+		default:
+			break;
+		}
+		alloc_end_time = get_cycles();
+		if (!kernel_mem)
+			break;
 
-        alloc_start_time = get_cycles();
-        switch (profile_mode)
-        {
-        case k_kmalloc:
-            kernel_mem = (char *)kmalloc(alloc_request, GFP_KERNEL);
-            break;
-        case k_vmalloc:
-            kernel_mem = (char *)vmalloc(alloc_request);
-            break;
-        case k_get_free_pages:
-            kernel_mem = (char *)__get_free_pages(GFP_KERNEL, order);
-            break;
-        case k_kzalloc:
-            kernel_mem = (char *)kzalloc(alloc_request, GFP_KERNEL);
-            break;
-        default:
-            break;
-        }
-        alloc_end_time = get_cycles();
-        if (!kernel_mem)
-            break;
+		pr_debug(
+			"Allocation:%s, Bytes:%ld, order: %ld, cycles for request: %lld\n",
+			alloc_function_name[profile_mode], alloc_request, order,
+			(alloc_end_time - alloc_start_time));
 
-        pr_debug("Allocation:%s, Bytes:%ld, order: %ld, cycles for request: %lld\n", alloc_function_name[profile_mode], alloc_request, order, (alloc_end_time - alloc_start_time));
+		freeing_start = get_cycles();
+		switch (profile_mode) {
+		case k_kmalloc:
+			kfree(kernel_mem);
+			break;
+		case k_vmalloc:
+			vfree(kernel_mem);
+			break;
+		case k_get_free_pages:
+			free_pages((unsigned long)kernel_mem, order);
+			break;
+		case k_kzalloc:
+			kfree(kernel_mem);
+			break;
+		default:
+			break;
+		}
+		freeing_end = get_cycles();
+		pr_debug(
+			"Freeing:%s, Bytes:%ld, order:%ld, cycles for request: %lld\n",
+			alloc_function_name[profile_mode], alloc_request, order,
+			(freeing_end - freeing_start));
 
-        freeing_start = get_cycles();
-        switch (profile_mode)
-        {
-        case k_kmalloc:
-            kfree(kernel_mem);
-            break;
-        case k_vmalloc:
-            vfree(kernel_mem);
-            break;
-        case k_get_free_pages:
-            free_pages((unsigned long)kernel_mem, order);
-            break;
-        case k_kzalloc:
-            kfree(kernel_mem);
-            break;
-        default:
-            break;
-        }
-        freeing_end = get_cycles();
-        pr_debug("Freeing:%s, Bytes:%ld, order:%ld, cycles for request: %lld\n", alloc_function_name[profile_mode], alloc_request, order, (freeing_end - freeing_start));
-
-        alloc_request *= 2;
-    } while (true);
+		alloc_request *= 2;
+	} while (true);
 }
 static int __init init(void)
 {
-    profile_alloc(k_kmalloc);
-    profile_alloc(k_vmalloc);
-    profile_alloc(k_get_free_pages);
-    profile_alloc(k_kzalloc);
+	profile_alloc(k_kmalloc);
+	profile_alloc(k_vmalloc);
+	profile_alloc(k_get_free_pages);
+	profile_alloc(k_kzalloc);
 
-    return -1;
+	return -1;
 }
 module_init(init);
 MODULE_AUTHOR("Valentyn Korniienko");
