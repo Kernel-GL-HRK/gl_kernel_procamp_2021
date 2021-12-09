4022f232e7332897b08ae7ef3053d8a5fb1373e7
Volodymyr Kharuk <jlabpih@ukr.net>
2021-12-09 02:15:41 +0200

    TASK03: Fix warnings from checkpatch
    Added commwent tag and removed spaces
    
    Signed-off-by: Volodymyr Kharuk <jlabpih@ukr.net>

--- 03_module/simple_module/smodule.c
+++ 03_module/simple_module/smodule.c
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0
+
 #include <linux/module.h>
 
 static int a = 1;
@@ -8,13 +10,13 @@ module_param(b, int, 0660);
 
 static int smodule_init(void)
 {
-    pr_info("SM init: a = %d, b = %d. a + b = %d", a, b, a + b);
-    return 0;
+	pr_info("SM init: a = %d, b = %d. a + b = %d", a, b, a + b);
+	return 0;
 }
 
 static void smodule_exit(void)
 {
-    pr_info("SM exit: a = %d, b = %d. a - b = %d", a, b, a - b);
+	pr_info("SM exit: a = %d, b = %d. a - b = %d", a, b, a - b);
 }
 
 module_init(smodule_init);
