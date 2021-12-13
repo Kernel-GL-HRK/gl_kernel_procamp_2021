fa937d820f6c203addb7ab4859cf92cf70ed1f45
Anatolii <tberchanov@gmail.com>
2021-12-13 23:41:18 +0200

    Task05: Add implementation to time_module. Implemented absolute and relative listeners. `module/logs.txt` attached.

--- 05_timers/module/time_module.c
+++ 05_timers/module/time_module.c
@@ -7,44 +7,30 @@
 #include <linux/sysfs.h>
 #include <linux/list.h>
 #include <linux/slab.h>
+#include <linux/timekeeping.h>
+#include <linux/ktime.h>
+#include <linux/time64.h>
 
-// struct node {
-// 	char *value;
-// 	struct list_head list_member;
-// };
-
-// static LIST_HEAD(nodeHead);
-
-// static void add_node(char *value, struct list_head *head)
-// {
-// 	pr_debug("---> %s", __func__);
-// 	struct node *nodePtr = kmalloc(sizeof(struct node), GFP_KERNEL);
-
-// 	if (nodePtr == NULL)
-// 		pr_err("Node creation error");
-// 	nodePtr->value = value;
-// 	INIT_LIST_HEAD(&nodePtr->list_member);
-// 	list_add(&(nodePtr->list_member), head);
-// 	pr_debug("---< %s", __func__);
-// }
+static ktime_t prev_time = 0;
+static ktime_t prev_abs_time = 0;
 
 static ssize_t relative_show(struct kobject *kobj, struct kobj_attribute *attr,
 	char *buf)
 {
 	pr_debug("---> %s", __func__);
-	// struct list_head *iter;
-	// struct node *nodePtr;
 
-	// int bufSize = 0;
+	if (prev_time == 0) {
+		prev_time = ktime_get();
+		pr_debug("Previous time initialization: %llu", prev_time);
+	} else {
+		ktime_t cur_time = ktime_get();
+		ktime_t delta = ktime_sub(cur_time, prev_time);
+		printk(KERN_INFO "Delta: %llu", delta);
+		prev_time = cur_time;
+		pr_debug("Previous time updated: %llu", prev_time);
+	}
 
-	// list_for_each(iter, &nodeHead) {
-	// 	nodePtr = list_entry(iter, struct node, list_member);
-	// 	strcpy(buf + bufSize, nodePtr->value);
-	// 	bufSize += strlen(nodePtr->value);
-	// 	pr_debug("Node copy to buf: %s", nodePtr->value);
-	// }
 	pr_debug("---< %s", __func__);
-	// return bufSize;
 	return 0;
 }
 
@@ -52,20 +38,24 @@ static ssize_t relative_store(struct kobject *kobj, struct kobj_attribute *attr,
 	const char *buf, size_t count)
 {
 	pr_debug("---> %s size_t: %ld", __func__, count);
-	// char *value = kcalloc(count, sizeof(char), GFP_KERNEL);
+	
+	if (sysfs_streq(buf, "reset")) {
+		pr_debug("Previous time reset: %llu", prev_time);
+		prev_time = 0;
+	}
 
-	// memcpy(value, buf, count);
-	// pr_debug("value created: %ld", strlen(value));
-	// add_node(value, &nodeHead);
 	pr_debug("---< %s", __func__);
-	// return count;
-	return 0;
+	return count;
 }
 
 static ssize_t absolute_show(struct kobject *kobj, struct kobj_attribute *attr,
 	char *buf)
 {
+	struct timespec64 ts;
 	pr_debug("---> %s", __func__);
+	ts = ktime_to_timespec64(prev_abs_time);
+	printk(KERN_INFO "Absolute: %lld.%09ld", ts.tv_sec, ts.tv_nsec);
+	prev_abs_time = ktime_get_real();
 	pr_debug("---< %s", __func__);
 	return 0;
 }
@@ -76,14 +66,14 @@ static ssize_t absolute_store(struct kobject *kobj, struct kobj_attribute *attr,
 	pr_debug("---> %s size_t: %ld", __func__, count);
 
 	pr_debug("---< %s", __func__);
-	return 0;
+	return count;
 }
 
 static struct kobj_attribute relative_attribute =
 	__ATTR(relative, 0664, relative_show, relative_store);
 
 static struct kobj_attribute absolute_attribute =
-	__ATTR(absolute, 0664, absolute_show, absolute_store);
+	__ATTR(absolute, 0444, absolute_show, absolute_store);
 
 static struct kobject *time_kobj;
 
@@ -115,16 +105,7 @@ static int time_init(void)
 static void time_exit(void)
 {
 	pr_debug("---> %s", __func__);
-	// struct node *nodePtr;
-	// struct node *tmp;
 
-	// list_for_each_entry_safe(nodePtr, tmp, &nodeHead, list_member) {
-	// 	pr_debug("Clean node: %s", nodePtr->value);
-	// 	kfree(nodePtr->value);
-	// 	list_del(&(nodePtr->list_member));
-	// 	kfree(nodePtr);
-	// }
-	// pr_debug("List_empty: %d", list_empty(&nodeHead));
 	kobject_put(time_kobj);
 
 	pr_debug("---< %s", __func__);
