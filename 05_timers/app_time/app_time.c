86f88d5279627924c3eaf4373b040e1782863b38
Volodymyr Kharuk <jlabpih@ukr.net>
2021-12-27 12:14:16 +0200

    TASK05: Remove different clock id
    Removed all clock id, as we need only absolute time.
    Added nicer time output.
    
    Signed-off-by: Volodymyr Kharuk <jlabpih@ukr.net>

--- 05_timers/app_time/app_time.c
+++ 05_timers/app_time/app_time.c
@@ -1,49 +1,21 @@
 #include <time.h>
 #include <stdio.h>
 
-int print_time(clockid_t id, char *name) {
-    struct timespec tp;
+int main() {
+    struct timespec tp, tr;
     int ret;
 
-    ret = clock_gettime(id, &tp);
+    ret = clock_gettime(CLOCK_REALTIME, &tp);
     if (ret < 0)
         return ret;
 
-    printf("Get time with id %s: %ld.%ld seconds", name, tp.tv_sec, tp.tv_nsec);
-
-    ret = clock_getres(id, &tp);
+    ret = clock_getres(CLOCK_REALTIME, &tr);
     if (ret < 0)
         return ret;
 
-    printf(" and time resolution is %ld nsec\n", tp.tv_nsec);
+	double ns = tp.tv_nsec / 1000000000.0;
+	double abs_time = tp.tv_sec + ns;
+    printf("Time is %.9f seconds, resolution is %ld nsec. Time local: %s", abs_time, tr.tv_nsec, ctime(&tp.tv_sec));
 
     return 0;
-}
-
-int main() {
-    int ret = 0;
-    
-    ret = print_time(CLOCK_REALTIME, "CLOCK_REALTIME");
-    if (ret < 0)
-        return ret;
-
-    ret = print_time(CLOCK_REALTIME_COARSE, "CLOCK_REALTIME_COARSE");
-    if (ret < 0)
-        return ret;
-
-    ret = print_time(CLOCK_MONOTONIC, "CLOCK_MONOTONIC");
-    if (ret < 0)
-        return ret;
-
-    ret = print_time(CLOCK_MONOTONIC_COARSE, "CLOCK_MONOTONIC_COARSE");
-    if (ret < 0)
-        return ret;
-
-    ret = print_time(CLOCK_MONOTONIC_RAW, "CLOCK_MONOTONIC_RAW");
-    if (ret < 0)
-        return ret;
-
-    ret = print_time(CLOCK_BOOTTIME, "CLOCK_BOOTTIME");
-
-    return ret;
 }
