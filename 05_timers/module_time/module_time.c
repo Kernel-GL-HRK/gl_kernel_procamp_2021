1cb6b97807100eb97eaead11d067aa961c600e16
Volodymyr Kharuk <jlabpih@ukr.net>
2022-01-03 15:13:32 +0200

    TASK05: Implement relative and absolute time logic
    Imeplemented reading absolute time from file /sys/kernel/my_object/abs_time.
    Imeplemented reading relative time from file /sys/kernel/my_object/rel_time.
    
    First read just return a warning that it was first read.
    Next reads will return the previous absolute time and relative time since the previous read.
    
    Signed-off-by: Volodymyr Kharuk <jlabpih@ukr.net>

--- 05_timers/module_time/module_time.c
+++ 05_timers/module_time/module_time.c
@@ -2,13 +2,44 @@
 
 #include <linux/module.h>
 
-static ssize_t my_time_show(struct kobject *kobj, struct kobj_attribute *attr,
+static int abs_first = 1;
+static struct timespec64 prev_abs_time;
+
+static ssize_t abs_time_show(struct kobject *kobj, struct kobj_attribute *attr,
 	char *buf)
 {
-	return scnprintf(buf, PAGE_SIZE, "hello");
+	if (abs_first) {
+		abs_first = 0;
+		return scnprintf(buf, PAGE_SIZE, "This one is first read of absolute time\n");
+	}
+
+	prev_abs_time = ktime_to_timespec64(ktime_get_real());
+	return scnprintf(buf, PAGE_SIZE, "Previous reading was at %lld.%09lds\n", prev_abs_time.tv_sec, prev_abs_time.tv_nsec);
 }
 
-static struct kobj_attribute my_attribute = __ATTR_RO_MODE(my_time, 0664);
+static int rel_first = 1;
+static ktime_t prev_rel_time;
+
+static ssize_t rel_time_show(struct kobject *kobj, struct kobj_attribute *attr,
+	char *buf)
+{
+	ktime_t rt_nxt;
+	struct timespec64 rt;
+
+	if (rel_first) {
+		rel_first = 0;
+		return scnprintf(buf, PAGE_SIZE, "This one is first read of relative time\n");
+	}
+
+	rt_nxt = ktime_get();
+	rt = ktime_to_timespec64(ktime_sub(rt_nxt, prev_rel_time));
+	prev_rel_time = rt_nxt;
+
+	return scnprintf(buf, PAGE_SIZE, "Time since previous reading %lld.%09lds\n", rt.tv_sec, rt.tv_nsec);
+}
+
+static struct kobj_attribute abs_time_attribute = __ATTR_RO_MODE(abs_time, 0664);
+static struct kobj_attribute rel_time_attribute = __ATTR_RO_MODE(rel_time, 0664);
 
 static struct kobject *my_object;
 
@@ -20,7 +51,11 @@ static int module_time_init(void)
 	if (!my_object)
 		return -ENOMEM;
 
-	ret = sysfs_create_file(my_object, &my_attribute.attr);
+	ret = sysfs_create_file(my_object, &abs_time_attribute.attr);
+	if (ret)
+		kobject_put(my_object);
+
+	ret = sysfs_create_file(my_object, &rel_time_attribute.attr);
 	if (ret)
 		kobject_put(my_object);
 
