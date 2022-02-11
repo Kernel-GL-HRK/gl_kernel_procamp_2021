97502788c9d8782cade437bacbdf8de0fc4457a3
ArtemShyshko <mr.artemshishko18@gmail.com>
2022-02-12 00:10:28 +0200

    TASK02: Modified hwdetect script
    hwdetect.sh script allows to detect plugged or unplugged devices

--- 02_bash/hwdetect/hwdetect.sh
+++ 02_bash/hwdetect/hwdetect.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-echo -e "Mounted devices: "
-ls /dev/sd*
-echo ""
+ls /dev/ > device_list
 
-echo -e "USB devices: "
-lsusb
-echo ""
+while :
+do
+    sleep 1
+	ls /dev/ > new_device_list
 
-echo -e "USB to TTL adapters: "
-ls /dev | grep ttyUSB
-echo ""
+	device_num=$(wc -l < device_list)
+	new_device_num=$(wc -l < new_device_list)
+	cmp=$(($device_num - $new_device_num))
 
-echo -e "SD cards: "
-lspci | grep -i SD
-echo ""
+	if [ $(($cmp + 0)) -ne 0 ]; then
+		if [ $(($device_num + 0)) -gt $(($new_device_num + 0)) ]; then
+		echo Unplugged device:
+		else
+		echo Plugged device:
+		fi
+		diff=1
+	else
+	diff=0
+	fi
 
-echo -e "I2C devices: "
-sudo i2cdetect -l
-echo ""
+	if [ $(($diff + 0)) -ne 0 ]; then
+		diff device_list new_device_list | awk '{print $2}'
+        echo ""
+		cp new_device_list device_list
+	fi
+done
