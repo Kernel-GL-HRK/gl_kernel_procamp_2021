1760d22841f999a14e70d8e59ce36ed655d1899f
ArtemShyshko <mr.artemshishko18@gmail.com>
2022-02-15 20:12:07 +0200

    TASK06: Implemented memory management task
    Following files were added:
    userspace_mem.c - a userspace program which allocates buffers using functions malloc, calloc, alloca. Time measurments contain in log.txt file for each function.
    mem_module.c - an implementation of a kernel module which allocates an frees memory using functions: kmalloc, kzmalloc, vmalloc, get_free_pages. Time measurments contain in log.txt file for each function.
    Separate Makefiles for each program, which contain targets for the module's build process and targets for testing. Target "test" creates a log.txt file for each pogramm.

---
+++ 06_memory/mem_module/mem_module.c
@@ -0,0 +1,115 @@
+#define pr_fmt(fmt) "%s: " fmt, KBUILD_MODNAME
+#define KMALLOC 1
+#define KZALLOC 2
+#define VMALLOC 3
+#define GET_FREE_PAGES 4
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/vmalloc.h>
+#include <linux/time.h>
+
+static int arg = 1;
+module_param(arg, int, 0660);
+
+int power(int, int);
+int get_nsec(struct timespec64);
+
+struct timespec64 clock;
+
+int init_module(void)
+{
+    int start, stop, diff_alloc, diff_free, pw, x;
+    pr_info("Module loaded\n");
+    for (x = 1; x <= 30; x++)
+    {
+        int* x_ptr;
+        long unsigned int addr; //for __get_free_pages
+        x_ptr = NULL;
+        pw = power(2, x);
+        start = get_nsec(clock);  //for allocating
+        switch (arg)
+        {
+            case KMALLOC:
+                x_ptr = kmalloc(pw, GFP_KERNEL);
+                break;
+            case KZALLOC:
+                x_ptr = kzalloc(pw, GFP_KERNEL);
+                break;
+            case VMALLOC:
+                x_ptr = vmalloc(pw);
+                break;
+            case GET_FREE_PAGES:
+                addr = __get_free_pages(GFP_KERNEL, x);
+                break;
+            default:
+                pr_info("Uncompatible argument");
+                return 1;
+                break;
+        }
+        stop = get_nsec(clock);
+        diff_alloc = stop - start;
+        start = get_nsec(clock);  //for freeing
+        if(arg != GET_FREE_PAGES)
+        {
+            if(arg != VMALLOC)
+            {
+                kfree(x_ptr);
+            }
+            else
+            {
+                vfree(x_ptr);
+            }
+            stop = get_nsec(clock);
+            diff_free = stop - start;
+            pr_info("%d ns for allocating and %d ns for freeing %d bytes\n", diff_alloc, diff_free, pw);
+        }
+        else
+        {
+            free_pages(addr, x);
+            stop = get_nsec(clock);
+            diff_free = stop - start;
+            pr_info("%d ns for allocating and %d ns for freeing %d pages\n", diff_alloc, diff_free, x);
+        }
+    }
+    return 0;
+}
+
+void cleanup_module(void)
+{
+    pr_info("Module exited\n");
+}
+
+int power(int base, int power)
+{
+    if (power == 0)
+    {
+        return 0;
+    }
+    else if (power == 1)
+    {
+        return base;
+    }
+    else
+    {
+        int count = 1, tmp = base;
+        while (count < power)
+        {
+            base *= tmp;
+            count++;
+        }
+    }
+    return base;
+}
+
+int get_nsec(struct timespec64 ts)
+{
+    int time;
+    ktime_get_real_ts64(&ts); 
+    time = ts.tv_nsec;
+    return time;
+}
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Artem Shyshko");
---
+++ 06_memory/userspace_mem/userspace_mem.c
@@ -0,0 +1,77 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include <malloc.h>
+
+#define MALLOC 1
+#define CALLOC 2
+#define ALLOCA 3
+
+int get_nsec(struct timespec ts)
+{
+    int time;
+    clock_gettime(CLOCK_REALTIME, &ts); 
+    time = ts.tv_nsec;
+    return time;
+}
+
+int main(int argc, char* argv[])
+{
+    struct timespec ts;
+    int start, stop, diff_alloc, diff_free;
+    float power;
+    if (argc > 1)
+    {
+        int arg = atoi(argv[1]);
+        if (arg != ALLOCA)
+        {
+            for (int x = 1; x <= 30; x++)
+            {
+                power = pow(2, x);
+                int* x_ptr = NULL;
+                start = get_nsec(ts);  //for allocating
+                switch (arg)
+                {
+                    case MALLOC:
+                        x_ptr = malloc(power);
+                        break;
+                    case CALLOC:
+                        x_ptr = calloc(power, sizeof(int));
+                        break;
+                    default:
+                        perror("Uncompatible argument");
+                        return 1;
+                        break;
+                } 
+                stop = get_nsec(ts);
+                diff_alloc = stop - start;
+                start = get_nsec(ts);  //for freeing
+                free(x_ptr);
+                stop = get_nsec(ts);
+                diff_free = stop - start;
+                printf("%d ns for allocating and %d ns for freeing %.0f bytes\n", diff_alloc, diff_free, power);
+            }
+        }
+        else    //if alloca chosen
+        {
+            for (int x = 1; x <= 21; x++)
+            {
+                power = pow(2, x);
+                int* x_ptr = NULL;
+                start = get_nsec(ts);
+                x_ptr = alloca(power);
+                stop = get_nsec(ts);
+                diff_alloc = stop - start;
+                printf("%d ns for allocating %.0f bytes\n", diff_alloc, power);
+            }
+            printf("alloca frees automatically\n");
+        }
+    }
+    else
+    {
+        perror("No arguments");
+        return 1;
+    }
+    return 0;
+}
