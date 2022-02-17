bba88ccbf610604a6299a64528b61f20db8aa5bf
ArtemShyshko <mr.artemshishko18@gmail.com>
2022-02-18 01:35:23 +0200

    TASK06: Fix format using checkpatch script

--- 06_memory/mem_module/mem_module.c
+++ 06_memory/mem_module/mem_module.c
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note
+
 #define pr_fmt(fmt) "%s: " fmt, KBUILD_MODNAME
 #define KMALLOC 1
 #define KZALLOC 2
@@ -13,102 +15,83 @@
 static int arg = 1;
 module_param(arg, int, 0660);
 
-int power(int, int);
-int get_nsec(struct timespec64);
+int power(int base, int power)
+{
+	if (power == 0) {
+		return 0;
+	int count = 1, tmp = base;
+
+	while (count < power) {
+		base *= tmp;
+		count++;
+	}
+	return base;
+}
+
+int get_nsec(struct timespec64 ts)
+{
+	int time;
+
+	ktime_get_real_ts64(&ts);
+	time = ts.tv_nsec;
+	return time;
+}
 
 struct timespec64 clock;
 
 int init_module(void)
 {
-    int start, stop, diff_alloc, diff_free, pw, x;
-    pr_info("Module loaded\n");
-    for (x = 1; x <= 30; x++)
-    {
-        int* x_ptr;
-        long unsigned int addr; //for __get_free_pages
-        x_ptr = NULL;
-        pw = power(2, x);
-        start = get_nsec(clock);  //for allocating
-        switch (arg)
-        {
-            case KMALLOC:
-                x_ptr = kmalloc(pw, GFP_KERNEL);
-                break;
-            case KZALLOC:
-                x_ptr = kzalloc(pw, GFP_KERNEL);
-                break;
-            case VMALLOC:
-                x_ptr = vmalloc(pw);
-                break;
-            case GET_FREE_PAGES:
-                addr = __get_free_pages(GFP_KERNEL, x);
-                break;
-            default:
-                pr_info("Uncompatible argument");
-                return 1;
-                break;
-        }
-        stop = get_nsec(clock);
-        diff_alloc = stop - start;
-        start = get_nsec(clock);  //for freeing
-        if(arg != GET_FREE_PAGES)
-        {
-            if(arg != VMALLOC)
-            {
-                kfree(x_ptr);
-            }
-            else
-            {
-                vfree(x_ptr);
-            }
-            stop = get_nsec(clock);
-            diff_free = stop - start;
-            pr_info("%d ns for allocating and %d ns for freeing %d bytes\n", diff_alloc, diff_free, pw);
-        }
-        else
-        {
-            free_pages(addr, x);
-            stop = get_nsec(clock);
-            diff_free = stop - start;
-            pr_info("%d ns for allocating and %d ns for freeing %d pages\n", diff_alloc, diff_free, x);
-        }
-    }
-    return 0;
+	int start, stop, diff_alloc, diff_free, pw, x;
+
+	pr_info("Module loaded\n");
+	for (x = 1; x <= 30; x++) {
+		int *x_ptr;
+		unsigned long addr; //for __get_free_pages
+
+		x_ptr = NULL;
+		pw = power(2, x);
+		start = get_nsec(clock);  //for allocating
+		switch (arg) {
+		case KMALLOC:
+			x_ptr = kmalloc(pw, GFP_KERNEL);
+			break;
+		case KZALLOC:
+			x_ptr = kzalloc(pw, GFP_KERNEL);
+			break;
+		case VMALLOC:
+			x_ptr = vmalloc(pw);
+			break;
+		case GET_FREE_PAGES:
+			addr = __get_free_pages(GFP_KERNEL, x);
+			break;
+		default:
+			pr_info("Uncompatible argument");
+			return 1;
+		}
+		stop = get_nsec(clock);
+		diff_alloc = stop - start;
+		start = get_nsec(clock);  //for freeing
+		if (arg != GET_FREE_PAGES) {
+			if (arg != VMALLOC)
+				kfree(x_ptr);
+			else
+				vfree(x_ptr);
+			stop = get_nsec(clock);
+			diff_free = stop - start;
+			pr_info("%d ns for allocating and %d ns for freeing %d bytes\n", diff_alloc, diff_free, pw);
+		} else {
+			free_pages(addr, x);
+			stop = get_nsec(clock);
+			diff_free = stop - start;
+			pr_info("%d ns for allocating and %d ns for freeing %d pages\n", diff_alloc, diff_free, x);
+		}
+	}
+	return 0;
 }
 
 void cleanup_module(void)
 {
-    pr_info("Module exited\n");
-}
-
-int power(int base, int power)
-{
-    if (power == 0)
-    {
-        return 0;
-    }
-    else if (power == 1)
-    {
-        return base;
-    }
-    else
-    {
-        int count = 1, tmp = base;
-        while (count < power)
-        {
-            base *= tmp;
-            count++;
-        }
-    }
-    return base;
-}
-
-int get_nsec(struct timespec64 ts)
-{
-    int time;
-    ktime_get_real_ts64(&ts); 
-    time = ts.tv_nsec;
-    return time;
+	pr_info("Module exited\n");
 }
 
 MODULE_LICENSE("GPL");
