15c4caf26982c48548082cc35ef228112226b0b8
ArtemShyshko <mr.artemshishko18@gmail.com>
2022-02-18 01:07:15 +0200

    TASK05: Fix format using checkpatch script

--- 05_timers/timer_module/timer_module.c
+++ 05_timers/timer_module/timer_module.c
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note
+
 #define pr_fmt(fmt) "%s: " fmt, KBUILD_MODNAME
 #define get_millisec(jiff) ((jiff * 1000) / HZ)
 #define EET_TIMEZONE 2
@@ -10,50 +12,42 @@
 #include <linux/time.h>
 
 bool is_first_read = true, is_abs = true;
-static int get_jiff = 0;
+static int get_jiff;
 struct timespec64 abs_clock;
 
 static ssize_t time_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
-    int diff;
-    diff = get_millisec(jiffies) - get_jiff;
-    get_jiff = get_millisec(jiffies);
-    if (is_abs)
-    {
-        ktime_get_real_ts64(&abs_clock);
-        sprintf(buf, "Absolute time: %.2llu:%.2llu:%.2llu:%.6lu\n",
-                (abs_clock.tv_sec / 3600) % (24) + (EET_TIMEZONE),
-                (abs_clock.tv_sec / 60) % (60),
-                abs_clock.tv_sec % 60,
-                abs_clock.tv_nsec / 1000);
-        is_first_read = false;
-    }
-    else
-    {
-        if (is_first_read)
-        {
-            sprintf(buf, "First read!\n");
-            is_first_read = false;
-        }
-        else
-        {
-            sprintf(buf, "%d milliseconds passed since previous read\n", diff);
-        }
-    }
-    return strlen(buf);
+	int diff;
+
+	diff = get_millisec(jiffies) - get_jiff;
+	get_jiff = get_millisec(jiffies);
+	if (is_abs) {
+		ktime_get_real_ts64(&abs_clock);
+		sprintf(buf, "Absolute time: %.2llu:%.2llu:%.2llu:%.6lu\n",
+				(abs_clock.tv_sec / 3600) % (24) + (EET_TIMEZONE),
+				(abs_clock.tv_sec / 60) % (60),
+				abs_clock.tv_sec % 60,
+				abs_clock.tv_nsec / 1000);
+		is_first_read = false;
+	} else {
+		if (is_first_read) {
+			sprintf(buf, "First read!\n");
+			is_first_read = false;
+		} else {
+			sprintf(buf, "%d milliseconds passed since previous read\n", diff);
+		}
+	}
+	return strlen(buf);
 }
 
 static ssize_t time_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
 {
-    if (buf[0] == '0')
-    {
-        is_abs = false;
-    }
-    else if (buf[0] == '1')
-    {
-        is_abs = true;
-    }
-    return count;
+	if (buf[0] == '0')
+		is_abs = false;
+	else if (buf[0] == '1')
+		is_abs = true;
+
+	return count;
 }
 
 static struct kobj_attribute time_attribute = __ATTR(time, 0664, time_show, time_store);
@@ -62,23 +56,23 @@ static struct kobject *time_kobj;
 
 static int time_init(void)
 {
-    int res = 0;
+	int res = 0;
 
-    time_kobj = kobject_create_and_add("time", kernel_kobj);
-    if (!time_kobj)
-        return -ENOMEM;
+	time_kobj = kobject_create_and_add("time", kernel_kobj);
+	if (!time_kobj)
+		return -ENOMEM;
 
-    res = sysfs_create_file(time_kobj, &time_attribute.attr);
-    if (res)
-        kobject_put(time_kobj);
-    pr_info("Module loaded\n");
-    return res;
+	res = sysfs_create_file(time_kobj, &time_attribute.attr);
+	if (res)
+		kobject_put(time_kobj);
+	pr_info("Module loaded\n");
+	return res;
 }
 
 static void time_exit(void)
 {
-    kobject_put(time_kobj);
-    pr_info("Module exited\n");
+	kobject_put(time_kobj);
+	pr_info("Module exited\n");
 }
 
 module_init(time_init);
