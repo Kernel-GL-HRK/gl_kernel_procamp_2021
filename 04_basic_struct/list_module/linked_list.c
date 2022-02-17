86883d70639e47547890088f355481c4cd542102
ArtemShyshko <mr.artemshishko18@gmail.com>
2022-02-18 00:57:08 +0200

    TASK04: Fix format using checkpatch script

--- 04_basic_struct/list_module/linked_list.c
+++ 04_basic_struct/list_module/linked_list.c
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note
+
 #define __NO_VERSION__
 
 #include <linux/module.h>
@@ -6,101 +8,103 @@
 
 linkedList *createList(char *initData)
 {
-    linkedList *headNode = (linkedList *)kmalloc(sizeof(linkedList), GFP_KERNEL);
-    headNode->data = initData;
-    headNode->next = NULL;
-    return headNode;
+	linkedList *headNode = (linkedList *)kmalloc(sizeof(linkedList), GFP_KERNEL);
+
+	headNode->data = initData;
+	headNode->next = NULL;
+	return headNode;
 }
 
 linkedList *addToStart(char *data, linkedList *headNode)
 {
-    linkedList *newNode = (linkedList *)kmalloc(sizeof(linkedList), GFP_KERNEL);
-    newNode->data = data;
-    newNode->next = headNode;
-    return newNode;
+	linkedList *newNode = (linkedList *)kmalloc(sizeof(linkedList), GFP_KERNEL);
+
+	newNode->data = data;
+	newNode->next = headNode;
+	return newNode;
 }
 
 linkedList *addToEnd(char *data, linkedList *headNode)
 {
-    if (headNode == NULL)
-    {
-        headNode = createList(data);
-        return headNode;
-    }
-    linkedList *newNode = (linkedList *)kmalloc(sizeof(linkedList), GFP_KERNEL);
-    newNode->data = data;
-    newNode->next = NULL;
-    linkedList *headPtr = headNode;
-    while (headPtr->next != NULL)
-    {
-        headPtr = headPtr->next;
-    }
-    headPtr->next = newNode;
+	if (headNode == NULL) {
+		headNode = createList(data);
+		return headNode;
+	}
+	linkedList *newNode = (linkedList *)kmalloc(sizeof(linkedList), GFP_KERNEL);
+
+	newNode->data = data;
+	newNode->next = NULL;
+	linkedList *headPtr = headNode;
+
+	while (headPtr->next != NULL)
+		headPtr = headPtr->next;
+	headPtr->next = newNode;
 }
 
 linkedList *add(char *data, int id, linkedList *headNode)
 {
-    if (headNode == NULL)
-    {
-        headNode = createList(data);
-        return headNode;
-    }
-    if (!id)
-    {
-        headNode = addToStart(data, headNode);
-        return headNode;
-    }
-    linkedList *headPtr = headNode;
-    headPtr = getNode(id, headPtr);
-    linkedList *newNode = (linkedList *)kmalloc(sizeof(linkedList), GFP_KERNEL);
-    newNode->data = data;
-    newNode->next = headPtr->next;
-    headPtr->next = newNode;
-    return headNode;
+	if (headNode == NULL) {
+		headNode = createList(data);
+		return headNode;
+	}
+	if (!id) {
+		headNode = addToStart(data, headNode);
+		return headNode;
+	}
+	linkedList *headPtr = headNode;
+
+	headPtr = getNode(id, headPtr);
+
+	linkedList *newNode = (linkedList *)kmalloc(sizeof(linkedList), GFP_KERNEL);
+
+	newNode->data = data;
+	newNode->next = headPtr->next;
+	headPtr->next = newNode;
+	return headNode;
 }
 
 linkedList *getNode(int id, linkedList *headNode)
 {
-    int count = 0;
-    while (count < id && headNode->next != NULL)
-    {
-        headNode = headNode->next;
-        count++;
-    }
-    return headNode;
+	int count = 0;
+
+	while (count < id && headNode->next != NULL) {
+		headNode = headNode->next;
+		count++;
+	}
+	return headNode;
 }
 
 void editNode(int id, char *newData, linkedList *headNode)
 {
-    headNode = getNode(id, headNode);
-    headNode->data = newData;
+	headNode = getNode(id, headNode);
+	headNode->data = newData;
 }
 
 void deleteList(linkedList *headNode)
 {
-    linkedList *headPtr;
-    while (headNode != NULL)
-    {
-        headPtr = headNode;
-        headNode = headNode->next;
-        kfree(headPtr);
-    }
+	linkedList *headPtr;
+
+	while (headNode != NULL) {
+		headPtr = headNode;
+		headNode = headNode->next;
+		kfree(headPtr);
+	}
 }
 
 linkedList *deleteNode(int id, linkedList *headNode)
 {
-    linkedList *headPtr;
-    if (!id)
-    {
-        headPtr = headNode->next;
-        kfree(headNode);
-        return headPtr;
-    }
-    headNode = getNode(id, headNode);
-    headPtr = headNode->next;
-    headNode->next = headPtr->next;
-    kfree(headPtr);
-    return headNode;
+	linkedList *headPtr;
+
+	if (!id) {
+		headPtr = headNode->next;
+		kfree(headNode);
+		return headPtr;
+	}
+	headNode = getNode(id, headNode);
+	headPtr = headNode->next;
+	headNode->next = headPtr->next;
+	kfree(headPtr);
+	return headNode;
 }
 
 MODULE_AUTHOR("Artem Shyshko");
--- 04_basic_struct/list_module/linked_list.h
+++ 04_basic_struct/list_module/linked_list.h
@@ -1,21 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
 #define NULL ((void *)0)
 
-typedef struct LinkedList
-{
-    char *data;
-    struct LinkedList *next;
+typedef struct LinkedList {
+	char *data;
+	struct LinkedList *next;
 } linkedList;
 
-linkedList *createList(char *);
-linkedList *addToStart(char *, linkedList *);
-linkedList *addToEnd(char *, linkedList *);
-linkedList *add(char *, int, linkedList *);
-linkedList *getNode(int, linkedList *);
-void editNode(int, char *, linkedList *);
-void deleteList(linkedList *);
-linkedList *deleteNode(int, linkedList *);
+linkedList *createList(char *initData);
+linkedList *addToStart(char *data, linkedList *headNode);
+linkedList *addToEnd(char *data, linkedList *headNode);
+linkedList *add(char *data, int id, linkedList *headNode);
+linkedList *getNode(int id, linkedList *headNode);
+void editNode(int id, char *newData, linkedList *headNode);
+void deleteList(linkedList *headNode);
+linkedList *deleteNode(int id, linkedList *headNode);
 
 #endif
